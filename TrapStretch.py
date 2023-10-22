3c51eca1d71af967afa5aa0ab39e66f57910383e
trulybrult <wyoksher@yandex.ru>
2023-10-22 03:20:55 +0300

    bug fix

--- TrapStretch.py
+++ TrapStretch.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import os
 def stretch_trap(trap, l=int(np.pi*480)):
     out_matrix = np.zeros_like(trap)
     for k in range(trap.shape[0]):
@@ -11,10 +11,9 @@ def stretch_trap(trap, l=int(np.pi*480)):
 def stretch_sequence_of_traps(path = 'E:/Tokodust/data_frames/8.1/'):
     trap_1 = cv2.imread(path + '000.png')
     out_image = stretch_trap(trap_1)
-    i = 1
-    for trap in traps[1:]:
+    file_count = sum(len(files) for _, _, files in os.walk(path))
+    for i in range(1,file_count):
         trap_1 = cv2.imread(path + '{:03d}.png'.format(i))
         out_image = cv2.vconcat([out_image, stretch_trap(trap_1)])
-        i+=1
     return out_image
 
